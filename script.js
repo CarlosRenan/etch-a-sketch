3a6baf8fd9ef1258a4c7cbb9b8694d71e3841137
CarlosRenan <renan.aragorn@gmail.com>
2019-08-31 00:43:18 -0300

    Change Pixel size working, new button back to default also working, styles missing

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
-    const gridProperties = document.getElementById("sketch");
-    const grid = document.querySelector(".container");
-    let gridSizeRows = gridProperties.offsetHeight;
-    let gridSizeColumns = gridProperties.offsetWidth;
-    let gridSizeDot = 20;
-    grid.style["grid-auto-rows"] = gridSizeDot + "px";
-    grid.style["grid-auto-columns"] = gridSizeDot + "px";
+const gridProperties = document.getElementById("sketch");
+const grid = document.querySelector(".container");
+let gridSizeRows = gridProperties.offsetHeight-6;
+let gridSizeColumns = gridProperties.offsetWidth-6;
+let size = 20;
 
-    /*function changeSize() {
-        let newSize = prompt('Please inform a even number between 8 and 100 to change the pixel size:');
-        if (newSize == gridSizeDot || newSize == null) {
-            alert("Nothing will change, the new size is the same as the old or you didn't type any number.");
-        } else if (newSize > 100 || newSize < 8) {
-            alert("The new pixel size must be a number between 8 and 100! Please Try again.");
-        } else {
-            resetGrid();
-            gridSizeDot = newSize;
-            recreateDivs(gridSizeRows, gridSizeColumns, gridSizeDot);
-        };
-    };*/
-    
-    function resetGrid() {
-        for (let i = 1; i < gridSizeRows/gridSizeDot; i++) {
-            for (let j = 1; j < gridSizeColumns/gridSizeDot; j++) {
-                let selectedDiv = document.getElementById("dot" + i + "-" + j);
-                selectedDiv.className = 'childs';
-            };
+//button functions
+
+function backToDefaults() {
+    size = 20;
+    resetGrid();
+    createDivs(gridSizeRows, gridSizeColumns, size);
+};
+
+function resetGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    };
+    createDivs(gridSizeRows, gridSizeColumns, size);
+};
+
+function changeSize() {
+    let oldSize = size;
+    size = prompt('Please inform a even number between 8 and 100 to change the pixel size:');
+    if (size == oldSize || size == null) {
+        alert("Nothing will change, the new size is the same as the old or you didn't type any number.");
+    } else if (size > 100 || size < 8) {
+        alert("The new pixel size must be a number between 8 and 100! Please Try again.");
+    } else {
+        resetGrid()
+        createDivs(gridSizeRows, gridSizeColumns, size);
+    };
+};
+  
+// end of button functions
+
+// grid pad creation
+
+const toAdd = document.createDocumentFragment();
+function createDivs(rowValue, columnValue, size){
+    for (let i = 1; i <= rowValue/size; i++) {
+        for (let j = 1; j <= columnValue/size; j++) {
+            let newDiv = document.createElement('div');
+            grid.style["grid-template-rows"] = size + "px";
+            grid.style["grid-template-columns"] = size + "px";
+            newDiv.className = "childs";
+            newDiv.id = "dot" + i + "-" + j;
+            newDiv.style.gridRowStart = i;
+            newDiv.style.gridRowEnd = i;
+            newDiv.style.gridColumnStart = j;
+            newDiv.style.gridColumnEnd = j;
+            newDiv.addEventListener('mouseover', (e) => {
+                newDiv.className = "childsblack";
+            });
+            toAdd.appendChild(newDiv);   
         };
     };
+    grid.append(toAdd);
+};
+createDivs(gridSizeRows, gridSizeColumns, size);
 
-    const toAdd = document.createDocumentFragment();
-    function createDivs(rowValue, columnValue, divider){
-        for (let i = 1; i < rowValue/divider; i++) {
-            for (let j = 1; j < columnValue/divider; j++) {
-                let newDiv = document.createElement('div');
-                newDiv.className = "childs";
-                newDiv.id = "dot" + i + "-" + j;
-                newDiv.style.gridRowStart = i;
-                newDiv.style.gridRowEnd = i;
-                newDiv.style.gridColumnStart = j;
-                newDiv.style.gridColumnEnd = j;
-                newDiv.addEventListener('mouseover', (e) => {
-                    newDiv.className = "childsblack";
-                });
-                toAdd.appendChild(newDiv);
-                
-            };
-        };
-        grid.append(toAdd);
-    };
-    createDivs(gridSizeRows, gridSizeColumns, gridSizeDot);
+// pad styles
